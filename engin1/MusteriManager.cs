097b765ca594c3ddcbb1e885c49f78ac00d8bc2c
Sultan Yeşilöz <dortsultan@users.noreply.github.com>
2022-08-17 21:58:18 +0300

    Add project files.

---
+++ engin1/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace engin1
+{
+    class MusteriManager
+    {
+        public void Ekle(musteri musteri)
+        {
+            Console.WriteLine("ID: " + musteri.Id + " Adı - Soyadı: " + musteri.Ad + musteri.Soyad + " olan müşteri sisteme eklenmiştir.");
+            Console.WriteLine("-------******------******");
+        }
+        public void Listele(musteri[] musteri)
+        {
+            foreach (musteri musteritem in musteri)
+            {
+
+                Console.WriteLine("ID: " + musteritem.Id + " Adı - Soyadı: " + musteritem.Ad + musteritem.Soyad);
+
+            }
+            Console.WriteLine("-------******------******");
+        }
+        public void Sil(musteri musteri)
+        {
+            Console.WriteLine("ID: " + musteri.Id + " Adı - Soyadı: " + musteri.Ad  + musteri.Soyad + " olan müşteri sistemden silinmiştir.");
+
+        }
+
+    }
+}
---
+++ engin1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace engin1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MusteriManager mManager = new MusteriManager();
+
+            musteri Musteri1 = new musteri();
+            Musteri1.Id = 11;
+            Musteri1.Ad = "Kamil";
+            Musteri1.Soyad = " ÖZEL";
+
+            musteri Musteri2 = new musteri();
+            Musteri2.Id = 10000002;
+            Musteri2.Ad = "Orkun";
+            Musteri2.Soyad = " DİNÇ";
+
+            musteri Musteri3 = new musteri();
+            Musteri3.Id = 1065663;
+            Musteri3.Ad = "Özgür";
+            Musteri3.Soyad = " DEMİRAY";
+
+            musteri[] MusteriArray = new musteri[] { Musteri1, Musteri2, Musteri3 };
+
+            mManager.Ekle(Musteri3);
+            mManager.Listele(MusteriArray);
+            mManager.Sil(Musteri2);
+
+
+
+        }
+    }
+}
